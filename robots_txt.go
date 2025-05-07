e6d209bf7b90f3cf124dc9e71c047c1749c444c2
Florent Poinsaut <florent@poinsaut.fr>
2025-05-07 08:01:33 +0000

    Add ai.robots.txt fetch

--- robots_txt.go
+++ robots_txt.go
@@ -5,6 +5,8 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 	"strings"
 )
@@ -12,60 +14,87 @@ import (
 // Config the plugin configuration.
 type Config struct {
 	AdditionalRules string `json:"headers,omitempty"`
-	LastModified    bool   `json:"lastModified,omitempty"`
+	AiRobotsTxt     bool   `json:"aiRobotsTxt,omitempty"`
 }
 
 // CreateConfig creates the default plugin configuration.
 func CreateConfig() *Config {
 	return &Config{
 		AdditionalRules: "",
+		AiRobotsTxt:     false,
 	}
 }
 
 type responseWriter struct {
-	buffer       bytes.Buffer
-	lastModified bool
-	wroteHeader  bool
+	buffer bytes.Buffer
 
 	http.ResponseWriter
 }
 
+// RobotsTxtPlugin a robots.txt plugin.
 type RobotsTxtPlugin struct {
 	additionalRules string
-	lastModified    bool
+	aiRobotsTxt     bool
 	next            http.Handler
 }
 
 // New created a new Demo plugin.
 func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
-	if len(config.AdditionalRules) == 0 {
-		return nil, fmt.Errorf("additionnal rules cannot be empty")
+	if len(config.AdditionalRules) == 0 && !config.AiRobotsTxt {
+		return nil, fmt.Errorf("set additionnal rules or set ai.robot.txt to true")
 	}
 
 	return &RobotsTxtPlugin{
 		additionalRules: config.AdditionalRules,
+		aiRobotsTxt:     config.AiRobotsTxt,
 		next:            next,
 	}, nil
 }
 
 func (p *RobotsTxtPlugin) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	wrappedWriter := &responseWriter{
-		lastModified:   p.lastModified,
 		ResponseWriter: rw,
 	}
 
 	if strings.HasSuffix(req.URL.Path, "/robots.txt") {
-
 		p.next.ServeHTTP(wrappedWriter, req)
 
 		body := wrappedWriter.buffer.String()
 
-		completeContent := body + p.additionalRules
+		if p.aiRobotsTxt {
+			aiRobotsTxt, err := p.fetchAiRobotsTxt()
+			if err != nil {
+				log.Printf("unable to fetch ai.robots.txt: %v", err)
+			}
+			body += aiRobotsTxt
+		}
+
+		body += p.additionalRules
 
 		rw.Header().Set("Content-Type", "text/plain")
 		rw.WriteHeader(http.StatusOK)
-		rw.Write([]byte(completeContent))
+		rw.Write([]byte(body))
 	} else {
 		p.next.ServeHTTP(rw, req)
 	}
+}
+
+func (p *RobotsTxtPlugin) fetchAiRobotsTxt() (string, error) {
+	backendURL := "https://raw.githubusercontent.com/ai-robots-txt/ai.robots.txt/refs/heads/main/robots.txt"
+
+	resp, err := http.Get(backendURL)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", nil // Aucun contenu existant, retourne une chaîne vide
+	}
+
+	content, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(content), nil
 }
--- robots_txt_test.go
+++ robots_txt_test.go
@@ -3,21 +3,23 @@ package traefik_plugin_robots_txt_test
 import (
 	"bytes"
 	"context"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	plugin "github.com/solution-libre/traefik-plugin-robots-txt"
 )
 
-func TestDemo(t *testing.T) {
+func TestAdditionalRules(t *testing.T) {
 	cfg := plugin.CreateConfig()
 	cfg.AdditionalRules = "\nUser-agent: *\nDisallow: /private/\n"
 
 	ctx := context.Background()
 	next := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {})
 
-	handler, err := plugin.New(ctx, next, cfg, "demo-plugin")
+	handler, err := plugin.New(ctx, next, cfg, "robots-txt-plugin")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -33,5 +35,50 @@ func TestDemo(t *testing.T) {
 
 	if !bytes.Equal([]byte("\nUser-agent: *\nDisallow: /private/\n"), recorder.Body.Bytes()) {
 		t.Errorf("got body %q, want %q", recorder.Body.Bytes(), "\nUser-agent: *\nDisallow: /private/\n")
+	}
+}
+
+func TestAiRobotsTxt(t *testing.T) {
+	cfg := plugin.CreateConfig()
+	cfg.AiRobotsTxt = true
+
+	ctx := context.Background()
+	next := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {})
+
+	handler, err := plugin.New(ctx, next, cfg, "robots-txt-plugin")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	recorder := httptest.NewRecorder()
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "http://localhost/robots.txt", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	handler.ServeHTTP(recorder, req)
+
+	if strings.HasSuffix(recorder.Body.String(), "Disallow: /") {
+		t.Errorf("got body %s, want terminated by %s", recorder.Body.String(), "Disallow: /")
+	}
+}
+
+func TestNoOption(t *testing.T) {
+	cfg := plugin.CreateConfig()
+	cfg.AdditionalRules = ""
+	cfg.AiRobotsTxt = false
+
+	ctx := context.Background()
+	next := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {})
+
+	_, err := plugin.New(ctx, next, cfg, "robots-txt-plugin")
+	if err == nil {
+		t.Fatal(fmt.Errorf("an error should raised up"))
+	} else {
+		errMsg := "set additionnal rules or set ai.robot.txt to true"
+		if err.Error() != errMsg {
+			t.Errorf("got err message %s, want %s", err.Error(), errMsg)
+		}
 	}
 }
